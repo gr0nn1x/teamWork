2276e6946a38771bbebcd1e2b6103a91fc75f8a3
Michal Carska <miscar@seznam.cz>
2024-05-07 09:19:18 +0200

    Added product

---
+++ client/src/pages/Products/Products.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Products() {
+  return (
+    <>
+    <p>Produkty bum bum bác, discorfd premium, uwuw owow tabwetw :3333333333333333</p>
+    <Link to="/">
+      <p>hlavní page</p>
+    </Link>
+    </>
+  );
+}
