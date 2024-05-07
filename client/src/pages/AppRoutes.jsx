b3d3bde768e6adf602377acef81a8f450534958d
VojtaKdo <Atjov05@seznam.cz>
2024-05-07 09:14:53 +0200

    Added AboutUs page

--- client/src/pages/AppRoutes.jsx
+++ client/src/pages/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./MainPage/MainPage";
 import Welcome from "./Welcome/Welcome";
+import AboutUs from "./AboutUs/AboutUs";
 
 export default function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/welcome" element={<Welcome />} />
+        <Route path="/aboutus" element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Welcome/Welcome.jsx
+++ client/src/pages/Welcome/Welcome.jsx
@@ -2,8 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function Welcome() {
   return (
-    <Link to="/">
-      <p>dve</p>
-    </Link>
+    <>
+      <Link to="/">
+        <p>dve</p>
+      </Link>
+      <Link to="/aboutus">
+        <p>About Us</p>
+      </Link>
+    </>
   );
 }
